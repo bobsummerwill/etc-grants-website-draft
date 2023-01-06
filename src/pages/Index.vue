0baf335b6c0f885681be48454d8c60dd97803272
Bob Summerwill <bob@summerwill.net>
2023-01-06 14:47:40 -0800

    Included all of Alison's process text
    Chunk of it still to be formatted.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,9 @@
   <Layout>
       <!-- Learn how to use images here: https://gridsome.org/docs/images -->
 
+    <hr/>
+    <a id="tldr"/>
+
     <div> <!-- TL;DR-->
       <h1>{{ $t("tldr.title") }}</h1>
       <p>
@@ -13,8 +16,10 @@
       </p>
     </div> <!-- /TL;DR-->
 
-    <div> <!-- Overview section-->
-      <hr/>
+    <hr/>
+    <a id="overview"/>
+
+    <div> <!-- Overview -->
       <h1>{{ $t("overview.title") }}</h1>
       <h2>{{ $t('overview.subheading')}}</h2>
       <p>
@@ -54,25 +59,79 @@
         2022 and will be contributing those funds into the program as well. Additional funds
         will be allocated by the Cooperative each calendar year.
       </p>
+    </div> <!-- /Overview -->
 
-      <h2>How is it going to work?</h2>
+    <hr/>
+    <a id="categories"/>
+
+    <div> <!-- Categories -->
+      <h1>Grants Streams</h1>
+
+      <h2>General grants</h2>
 
       <p>
-        The grants process will be organized into different rounds of funding applications,
-        starting in January 2023.  A timetable and details will be announced at the
-        opening of each round, and the details will vary over time as we learn from
-        previous rounds.
+        General grants are open to all and any teams looking to build their projects on ETC. These grants do not have an open request by the ETC grant committee, but can prove they would provide value to the ETC ecosystem. There are suggested topics and ideas, on the ETC grants website, but without formal requests for proposals.
       </p>
-      <p>
-        The first round will on the longer side as we find our feet.
-      </p>
-      <p>
-        This website and associated social media channels will be used for all coordination
-        around the grants process.
-      </p>
-    </div>
 
-    <a id="Requirements"/>
+      <p>
+        The maximum funding per team for open grants is generally $50,000, unless applying under a community grant (maximum of $10,000) or a premium grant (those over $50,000).
+      </p>
+
+      <h2>Targeted Grants</h2>
+
+      <p>
+        Targeted grants are those that have been selected by the grant selection committee and community as priority items and growth areas and have an open request. These items may or may not change each grant round, as priorities change and teams launch their projects. There may not be any open requests, at the beginning of a new round. In that case, General grants should be selected for the category.
+      </p>
+
+      <p>
+        The maximum funding per team for targeted grants is generally $50,000, however teams can apply for premium grants which are over $50,000, if they meet the criteria. 
+      </p>
+
+      <p>
+        As part of the targeted grants, there may also be an open request for critical path items, where the ETC grants team will select the companies or teams to submit their application for a specific and defined piece of work. These projects would be those considered critical to moving ETC forward and would pave the way for future growth. 
+      </p>
+
+      <h1>Grants Types</h1>
+
+      <p>
+        <table>
+          <tr>
+            <th>Type</th>
+            <th>Funding Maximum</th>
+            <th>Category</th>
+            <th>Description</th>
+          </tr>
+          <tr>
+            <td>Community Grant</td>
+            <td>Up to $10,000</td>
+            <td>Targeted grant or General</td>
+            <td>Grassroots community grants such as hosting presentations, meet-ups, writing articles, starting clubs, translations, supporting aligned non-profits, using ETC within an IRL setting, supporting start-ups in underserved markets, etc.</td>
+          </tr>
+          <tr>
+            <td>Conversion Grant</td>
+            <td>$25,000</td>
+            <td>General</td>
+            <td>A Conversion Grant would be for project teams that have successfully launched a viable product on other blockchains and want to launch the same project on ETC.</td>
+          </tr>
+          <tr>
+            <td>Standard Grant</td>
+            <td>Up to $50,000</td>
+            <td>Targeted grant or General</td>
+            <td>Most grants would fall into this bucket. The team would be expected to be experienced in launching projects, have the relevant technical experience to perform work required, and have a detailed breakdown of the project. These grants can be targeted with an open RFP or open grants.</td>
+          </tr>
+          <tr>
+            <td>Premium Grant</td>
+            <td>Over $50,000</td>
+            <td>Targeted grant or General</td>
+            <td>These grants would only be available to teams with a proven track record of success. The project team would need to provide clear evidence of successful past launches and why funding over the amount of a standard grant, would be needed to meet objectives.  If a team requests this type of grant and the grant committee likes the project, but does not feel it meets the stringent requirements of a premium grant, they may accept the grant under the terms of the Standard grant. The project team would then have the right to accept or decline during the grant approval phase.
+            </td>
+          </tr>
+        </table>     
+      </p>
+
+    </div> <!-- /Categories-->
+
+    <a id="requirements"/>
     <div> <!-- Requirements -->
       <hr/>
       <h1>What do grant applications need to know?</h1>
@@ -149,23 +208,96 @@
         are likely to have something more primitive at first.
       </p>
 
-      <h2>Progress Reports</h2>
-      <p>
-        Applicants are obliged to provide two status reports.
-        The first report will be mid-project and the second report is due at
-        the completion of the project, or when work towards its stated goals
-        comes to an end for whatever reason.
-      </p>
-      <p>
-        These reports will all be made public, as will the proposals themselves,
-        together with feedback and rationale from the committee on each of the
-        proposals explaining how their merits were assessed and how that
-        assessment informed the decision-making process for allocation of funds.
-      </p>
     </div> <!-- /Requirements -->
 
+    <hr/>
+    <a id="grantsprocess"/>
+
+    <div> <!-- Grants process -->
+
+      <h1>ETC Grants Process</h1>
+
+      <p>
+Grant Process
+Call for proposals 🡪 Grant Application submission 🡪 Evaluation/Technical review 🡪 Grants team approval 🡪 Formal agreement & Due diligence (KYC/KYB) 🡪 Milestone reviews & payments 🡪 Project Launch & Project retrospective analysis.
+
+Timeline 
+
+Announcement of new grant round - Call for proposals
+Grant application submissions open (3 months)
+Evaluation/Technical review, approval and due diligence (1-3 months)
+Milestone reviews and project launch will be done on a project by project basis. It should be outlined in the application, when milestones and deliverables are expected. This part of the grant process will run concurrently, with the launch of subsequent grant rounds. (No defined timeline - each project will have a different timeline). 
+
+
+
+The 7 step grant process is outlined below:
+
+Step 1: Call for Proposals
+At the beginning of each grant round, the ETC grant committee will formally open the round with a call for proposals. The documentation on the grant site and other marketing materials , will outline the exact timeline for each grant round, for the following phases:
+The application submission (generally 3 months, subject to change)
+The grant committee review and approval (generally one month, subject to change)
+The call for proposals documentation will also identify the open requests for that specific grant round and teams will identify if they are submitting their application, under one of these open requests as well as the type of grant they are applying for. 
+
+Step 2:  Grant Application Submission 
+Grant applications should be submitted via the site (Airtable). The application will outline all relevant details each team or individual should include. People should enter as much information as possible about their project, providing clear milestones and deliverables. 
+In the grant application, teams will specify which open request they are applying for or if they are applying under the general grant framework.
+When submitting the application, you will choose one of the grant types. You will need to ensure that your requested grant funding does not exceed the guidelines for the grant type. To receive a premium grant, a team will be expected to have a clear financial break-down, proven track record of success, and clearly demonstrate the value proposition to the grants committee. 
+
+Step 3: Evaluation/Technical review
+Currently, the ETC Grant committee is made up of the following:
+Representative from AntPool
+Representative from Bitmain Xmei
+Representative from ETC Coop (Bob Summerwill, Executive Director)
+Xxxx
+In the future, once the ecosystem has grown there will be (number xx) of rotating grant committee seats, that will take part in the grant selection process. This will both encourage more diverse ecosystem engagement but also provide an incentive for projects based on their organic growth, to take part in further growing the ecosystem. 
+During the evaluation and technical review the grant committee will look at all the information submitted by each team and evaluate the application. Please note that the voting for the projects will be done by the members listed above, but the technical evaluation of the projects will be done by various stakeholders, with several members of the ETC Cooperative’s technical staff taking part. This is to ensure that there is sufficient technical review of all applications. 
+For each open request there may be grants awarded to multiple teams, one team, or no teams if none of the submissions meet the standards required. 
+Your project will have better chances for acceptance if the following are met:
+The team has proven experience and strong technical background.
+The team can demonstrate that they have a business model that will sustain the project long term. If the project is an open source public good and thus there are no financial incentives for the team or to sustain the project, the application should be clear on how much annual funding would be required to maintain the project. Funding beyond the initial grant, is not guaranteed and the team would have to apply for grants in subsequent periods. 
+Your application clearly presents milestones and how to evaluate whether KPI’s have been met or not. 
+Your project fills an obvious void in the current ecosystem.
+Your project provides clear value to the ETC ecosystem.
+
+Reasons a project may be declined:
+Project is not aligned with ETC’s values.
+Project doesn’t have a way to sustain itself after grant funding has been fully utilized (if it is not an open source public good type grant).
+Project is not open-source.
+Project team does not appear to have requisite expertise to accomplish project goals.
+Project funding required is more than maximum grants for that grant category and type.
+The Cost/benefit of the project appears negative. 
+During this phase, the grant committee team may reach out for more information regarding your project and conduct an interview. If your project is already at a testing phase, we may schedule a time to demonstrate the project and for the committee members and other involved stakeholders to review and ask questions.
+As the grant program matures more feedback will be provided on what quantitative and qualitative factors grants will be evaluated against. 
+
+Step 4: Grants team approval
+After evaluation of all submitted grants (within the submission timeline), an applicant will be notified about whether their application has been accepted or denied. If an application was denied, the team will receive a notification with some feedback on why. If an application was approved, the team will move into the due diligence phase.  
+
+
+Step 5: Due diligence and AML/KYC requirements
+If your grant application has been accepted, you will go through due diligence and sign your formal agreement, which includes KYC/KYB required information. This information will be obtained by a third party independent (Trulioo) company to comply with all AML and KYC requirements. The final agreement will specify terms of the contract and required milestones to release milestone payments. Agreements would be signed by all relevant parties. 
+Once all successful applicants have completed the due diligence and rejected projects, the grant committee team will announce that round's grantees. The amount of funding committed in the round will also be disclosed. The plan is to have an active directory tab on the ETC grant website, that will show the status of all applications. This may not be achievable for the first grant round. 
+
+Step 6: Grant progress and Milestone review
+During this stage of the process, the grantee would submit milestone/progress reports.  These reports will provide insight to the grant committee and the community, about the progress of the individual grant. It will also be used to assess the efficacy and robustness of the ETC grant-making program and process. These reports will be made public, for transparency. 
+The documentation will depend on whether it is a larger project, that has clear and differentiated milestones, or a smaller project without discrete pieces of work. If it is the latter, then a grant progress report template will be completed mid-way through the grant period, to assess the progress of the project. 
+Larger projects, that have specific milestones, would be required to submit a milestone progress report, after completing each milestone. Funding will occur after each milestone has been performed and reviewed. 
+During this phase, project evaluations and KPI’s should be documented, as well as any changes in scope. In order to release milestone payments, it should be clear to the grant committee that these milestones were in fact met and that the project is on track. If at any milestones project review, it is determined that the project will not be able to achieve its final targets or objectives, the team should have formal discussions with the grant committee about whether a change of scope is needed or if the project should be abandoned. 
+If milestones are not met or do not meet expectations, or continuing the project does not make sense, work will be stopped and a post-mortem analysis will occur at that point. If a project goes to completion, post mortem analysis will commence at the end of the project. 
+Payments  
+Projects are expected to submit their invoice for milestone payments and the grant team, administering the payment, will calculate the ETC to be paid. More information will be provided during the grant acceptance phase. 
+
+Step 7: Project Launch and Retrospective
+This phase is the final step in the grant making process and if the project has been successful, will lead to the launch of the final project and/or deliverables, as well as the completion milestone payment. This phase commences with the project retrospective that provides post mortem analysis to determine and analyze elements of the project that were successful or unsuccessful. 
+Project post-mortems are intended to inform process improvements which mitigate future risks and to promote iterative best practices. Post-mortems are often considered a key component of, and ongoing precursor to, effective risk management. A grant project post mortem will be required on all projects, as this will allow the grant team to tweak the process as the program matures. 
+As this is a new process, it will be regularly evolving to take into account learnings from each round and to integrate feedback from participants and the community. Please keep in mind that the ETC grant committee and grant admin teams are not as well resourced as many other crypto grant programs. We aim to make the processes as clear and transparent as possible and will adapt as needed.
+
+        </p>
+    </div> <!-- /Grants process -->
+
+    <hr/>
+    <a id="decisionmaking"/>
+
     <div> <!-- Decision making -->
-      <hr/>
       <h1>Decision making for the ETC Grants Program</h1>
 
       <h2>Who will be making the decisions?</h2>
@@ -190,6 +322,8 @@
     </div> <!-- /Decision making -->
 
     <hr/>
+    <a id="round1"/>
+
     <div> <!-- Round 1 -->
       <h1>Round 1 of the ETC Grants Program</h1>
       <h2>What kind of projects are you looking to fund?</h2>
@@ -228,50 +362,6 @@
         The final decisions will be announced and initial funds disbursed in May 2023.
       </p>
 
-      <h2>How much funding is available in Round 1?</h2>
-      <p>
-        Up to $1M USD worth of ETC is available for successful applicants in
-        this first round of grants, which was funded by Antpool ($750K)
-        and ETC Coop ($250K).
-      </p>
-      <p>
-        The maximum funding per team for open grants is generally $50,000, unless applying under a community grant (maximum of $10,000) or a premium grant (those over $50,000).
-      </p>
-
-      <table>
-        <tr>
-          <th>Type</th>
-          <th>Funding Maximum</th>
-          <th>Category</th>
-          <th>Description</th>
-        </tr>
-        <tr>
-          <td>Community Grant</td>
-          <td>Up to $10,000</td>
-          <td>Targeted grant or General</td>
-          <td>Grassroots community grants such as hosting presentations, meet-ups, writing articles, starting clubs, translations, supporting aligned non-profits, using ETC within an IRL setting, supporting start-ups in underserved markets, etc.</td>
-        </tr>
-        <tr>
-          <td>Conversion Grant</td>
-          <td>$25,000</td>
-          <td>General</td>
-          <td>A Conversion Grant would be for project teams that have successfully launched a viable product on other blockchains and want to launch the same project on ETC.</td>
-        </tr>
-        <tr>
-          <td>Standard Grant</td>
-          <td>Up to $50,000</td>
-          <td>Targeted grant or General</td>
-          <td>Most grants would fall into this bucket. The team would be expected to be experienced in launching projects, have the relevant technical experience to perform work required, and have a detailed breakdown of the project. These grants can be targeted with an open RFP or open grants.</td>
-        </tr>
-        <tr>
-          <td>Premium Grant</td>
-          <td>Over $50,000</td>
-          <td>Targeted grant or General</td>
-          <td>These grants would only be available to teams with a proven track record of success. The project team would need to provide clear evidence of successful past launches and why funding over the amount of a standard grant, would be needed to meet objectives.  If a team requests this type of grant and the grant committee likes the project, but does not feel it meets the stringent requirements of a premium grant, they may accept the grant under the terms of the Standard grant. The project team would then have the right to accept or decline during the grant approval phase.
-</td>
-        </tr>
-      </table>      
-
       <h2>Ready to go?</h2>
       <p>
         Bring your best ideas!
@@ -280,6 +370,8 @@
 
       <br/>
     </div> <!-- /Round 1 -->
+
+
 
     <!-- Commented out original template for the home page, with localization support.
          For now I've just cut-and-pasted all the English text from the specific pages
